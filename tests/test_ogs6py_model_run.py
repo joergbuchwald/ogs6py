a5634816185dd1513be450931f8a822b0a058a02
OliverPe <oliver.petschick@gmail.com>
2021-10-11 14:58:09 +0200

    now testing on specific runtimeError messages

--- tests/test_ogs6py_model_run.py
+++ tests/test_ogs6py_model_run.py
@@ -17,24 +17,31 @@ class TestiOGSModelrun(unittest.TestCase):
         
         # case: path is not a dir
         model = ogs6py.OGS(INPUT_FILE=prjfile, PROJECT_FILE=prjfile)
-        with self.assertRaises(RuntimeError):
-            model.run_model(container=True, path="not/a/dir")
+        with self.assertRaises(RuntimeError) as cm:
+            model.run_model(path="not/a/dir", container_path=sif_file.name)
+        self.assertEqual('The specified path is not a directory. Please provide a directory containing the Singularity executable.',
+            str(cm.exception))
         # case: container_path is not a file:
-        with self.assertRaises(RuntimeError):
-            model.run_model(container=True, container_path="not/a/file")
+        with self.assertRaises(RuntimeError) as cm:
+            model.run_model(container_path="not/a/file")
+        self.assertEqual('The specific container-path is not a file. Please provide a path to the OGS container.',
+            str(cm.exception))
         # case: container_path is not a *.sif file
-        with self.assertRaises(RuntimeError):
-            model.run_model(container=True, container_path=x_file.name)
-        # case: container_path is missing
-        with self.assertRaises(RuntimeError):
-            model.run_model(container=True)
+        with self.assertRaises(RuntimeError) as cm:
+            model.run_model(container_path=x_file.name)
+        self.assertEqual('The specific file is not a Singularity container. Please provide a *.sif file containing OGS.',
+            str(cm.exception))
         # case Singularity executable not found without path
         if shutil.which(os.path.join("singularity")) is None:
-            with self.assertRaises(RuntimeError):
-                model.run_model(container=True, container_path=sif_file.name)
+            with self.assertRaises(RuntimeError) as cm:
+                model.run_model(container_path=sif_file.name)
+            self.assertEqual('The Singularity executable was not found. See https://www.opengeosys.org/docs/userguide/basics/container/ for installation instructions.',
+                str(cm.exception))
         # case Singularity executable not found in path
-        with self.assertRaises(RuntimeError):
-            model.run_model(container=True, path=sing_dir.name, container_path=sif_file.name)
+        with self.assertRaises(RuntimeError) as cm:
+            model.run_model(path=sing_dir.name, container_path=sif_file.name)
+        self.assertEqual('The Singularity executable was not found. See https://www.opengeosys.org/docs/userguide/basics/container/ for installation instructions.',
+            str(cm.exception))
         
         # clean up the temporary dir
         sing_dir.cleanup()
