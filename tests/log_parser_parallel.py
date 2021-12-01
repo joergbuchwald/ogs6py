1736a5bd5c1ef1653bc43d6c8dbd9dc405355e0a
Tobias Meisel <tobias.meisel@ufz.de>
2021-12-01 10:48:42 +0100

    Log parser: Introduce tests for convergence in staggered scheme

--- tests/log_parser_parallel.py
+++ tests/log_parser_parallel.py
@@ -4,7 +4,9 @@ from ogs6py.log_parser.log_parser import parse_file
 # this needs to be replaced with regexes from specific ogs version
 from ogs6py.log_parser.ogs_regexes import ogs_regexes
 from collections import namedtuple, defaultdict
-from ogs6py.log_parser.common_ogs_analyses import pandas_from_records, analysis_by_time_step
+from ogs6py.log_parser.common_ogs_analyses import pandas_from_records, analysis_by_time_step, \
+    analysis_convergence_newton_iteration, analysis_convergence_coupling_iteration
+
 
 def log_types(records):
     d = defaultdict(list)
@@ -53,20 +55,34 @@ class OGSParserTest(unittest.TestCase):
         self.assertAlmostEqual(dfe.at[record_id(mpi_process=2.0, time_step=1.0), 'time_step_solution_time'], 0.008504,
                                digits)
 
-    def test_seriel_convergence_short(self):
-        filename = 'parser/serial_convergence_short.txt'
+    def test_serial_convergence_newton_iteration_long(self):
+        filename = 'parser/serial_convergence_long.txt'
         records = parse_file(filename, ogs_regexes(), petsc=False)
         df = pandas_from_records(records)
-        # component-wise
-        dfe_sic = df.pivot_table(['dx', 'x', 'dx_x'], ['time_step', 'iteration_number', 'component'])
-        # iteration - wise
-        dfe_si = df.pivot_table(['dx', 'x', 'dx_x'], ['time_step', 'iteration_number'])
+        dfe = analysis_convergence_newton_iteration(df)
 
         # some specific values
-        record_id = namedtuple('id', 'time_step iteration_number component')
+        record_id = namedtuple('id', 'time_step coupling_iteration process iteration_number component')
         digits = 6
-        self.assertAlmostEqual(dfe_sic.at[record_id(time_step=1.0, iteration_number=1, component=1), 'dx'],
-                               9.694038e-02, digits)
+        self.assertAlmostEqual(dfe.at[record_id(time_step=1.0, coupling_iteration=0, process=0, iteration_number=1, component=1), 'dx'],
+                               1.935000e-01, digits)
+        self.assertAlmostEqual(dfe.at[record_id(time_step=10.0, coupling_iteration=5, process=1, iteration_number=1, component=-1), 'x'],
+                               1.289000e+07, digits)
+
+    def test_serial_convergence_coupling_iteration_long(self):
+        filename = 'parser/serial_convergence_long.txt'
+        records = parse_file(filename, ogs_regexes(), petsc=False)
+        df = pandas_from_records(records)
+        dfe = analysis_convergence_coupling_iteration(df)
+
+        # some specific values
+        record_id = namedtuple('id', 'time_step coupling_iteration coupling_iteration_process component')
+        digits = 6
+        self.assertAlmostEqual(dfe.at[record_id(time_step=1.0, coupling_iteration=1, coupling_iteration_process=0,  component=-1), 'dx'],
+                               1.696400e+03, digits)
+        self.assertAlmostEqual(dfe.at[record_id(time_step=10.0, coupling_iteration=5, coupling_iteration_process=1,  component=-1), 'x'],
+                               1.066500e+00, digits)
+
 
 
 if __name__ == '__main__':
