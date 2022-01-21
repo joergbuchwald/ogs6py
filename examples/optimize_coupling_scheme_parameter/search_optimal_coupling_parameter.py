8d5ba91fa3bbe6d8ce6c7c4fa6684b2370630785
Dominik Kern <dominik.kern.ifgt@gmail.com>
2022-01-21 11:20:36 +0100

    add documentation header

--- examples/optimize_coupling_scheme_parameter/search_optimal_coupling_parameter.py
+++ examples/optimize_coupling_scheme_parameter/search_optimal_coupling_parameter.py
@@ -3,6 +3,14 @@
 """
 Created on Mon Dec 20 13:58:50 2021
 
+This examples shows how to optimize the coupling parameter for a staggered scheme.
+The staggered scheme is the fixed-stress split for hydromechanical systems.
+
+More information about
+- staggered scheme [Mikelic, Wheeler: Convergence of iterative coupling for coupled flow and geomechanics]
+- coupling parameter [Storvik, Both, Kumar, Nordbotten, Radu: On the optimization of the fixed-stress splitting for Biot's equations]
+- example model [Kim, Tchelepi: Stability, Accuracy and Efficiency of Sequential Methods for Coupled Flow and Geomechanics]
+
 @author: dominik kern (tu freiberg)
 """
 from search_toolbox import coupling_parameter_search
