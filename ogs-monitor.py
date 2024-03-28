d672d6538e09740a7e26e99b5386b7d8c1b88765
Jörg Buchwald <joerg.buchwald@ufz.de>
2024-03-28 17:25:20 +0100

    use records instead of df

--- ogs-monitor.py
+++ ogs-monitor.py
@@ -36,6 +36,7 @@ class OGSMONITOR(object):
         self.running = True
         self.show_quadrant = int(show_quadrant)
         self.filters = ["time_step_vs_iterations","by_time_step", "convergence_newton_iteration","analysis_simulation"]
+        """
         self.filterdict = {"by_time_step":parse_fcts.analysis_time_step,
                 "convergence_newton_iteration":parse_fcts.analysis_convergence_newton_iteration,
                 "convergence_coupling_iteration": parse_fcts.analysis_convergence_coupling_iteration,
@@ -43,18 +44,30 @@ class OGSMONITOR(object):
                 "analysis_simulation": parse_fcts.analysis_simulation,
                 "fill_ogs_context": parse_fcts.fill_ogs_context
                 }
-        self.logfile_ref_df = {}
+        """
+        # plot data from records actual
+        self.last_record = {"actual": 0, "ref": 0}
+        self.last_line = {"actual": 0, "ref": 0}
+        self.timesteps = {"actual": [], "ref": []}
+        self.ts_simtime = {"actual": [], "ref": []}
+        self.iterations_per_ts = {"actual": [], "ref": []}
+        self.assembly_solver_time = {"actual": [], "ref": []}
+        self.criterion_x = {"actual": [], "ref": []}
+        self.criterion_label = {"actual": [], "ref": []}
+        self.criterion_y = {"actual": [], "ref": []}
+        self.crit_timesteppointer = {"actual": [], "ref": []}
+
         if self.ref_file is not None:
             records_ref, _ = parser.parse_file(self.ref_file, maximum_lines=self.maximum_lines, force_parallel=False)
+            self.parse_records(records_ref, dataset="ref")
+            """
             df_ref = pd.DataFrame(records_ref)
             df_ref = parse_fcts.fill_ogs_context(df_ref)
             for j, filt in enumerate(self.filters):
                 self.logfile_ref_df[j] = self.filterdict[filt](df_ref)
                 self.logfile_ref_df[j].reset_index(inplace=True)
-        else:
-            for j, filt in enumerate(self.filters):
-                self.logfile_ref_df[j] = None
-                self.q0_xb = None
+            """
+        self.q0_xb = None
         self.q0_yb = None
         self.q1_xb = None
         self.q1_yb = None
@@ -62,40 +75,40 @@ class OGSMONITOR(object):
         self.q2_yb = None
         self.q3_xb = None
         self.q3_yb = None
-        self.last_record = 0
-        self.last_line = 0
-        #TimeStepStartTime record
-        self.timesteps = []
-        self.ts_simtime = []
-        self.iterations_per_ts = []
-        self.assembly_solver_time = []
-        self.criterion = []
+        # plot data from records actual
         self.plot_init()
         self.plot()
 
-    def parse_records(self, records):
-        len_records = len(records)
-        for record in records[self.last_record:len_records]:
+    def parse_records(self, records, dataset="actual"):
+        #len_records = len(records)
+        for record in records: #[self.last_record[dataset]:len_records]:
             if isinstance(record, ogs6py.ogs_regexes.ogs_regexes.TimeStepStartTime):
-                self.timesteps.append(record.time_step)
-                self.ts_simtime.append((record.step_start_time, record.step_size))
-                self.iterations_per_ts.append(0)
-                self.assembly_solver_time.append([0, 0])
+                self.timesteps[dataset].append(int(record.time_step))
+                self.ts_simtime[dataset].append((float(record.step_start_time), float(record.step_size)))
+                self.iterations_per_ts[dataset].append(1)
+                self.assembly_solver_time[dataset].append([0, 0])
 
             elif isinstance(record, ogs6py.ogs_regexes.ogs_regexes.IterationTime):
-                self.iterations_per_ts[-1] = record.iteration_number
+                self.iterations_per_ts[dataset][-1] = int(record.iteration_number)+1
             elif isinstance(record, ogs6py.ogs_regexes.ogs_regexes.AssemblyTime):
-                self.assembly_solver_time[-1][0] = (self.assembly_solver_time[-1][0] +
-                                                    record.assembly_time) / (1. + self.iterations_per_ts[-1])
-                print(self.assembly_solver_time[-1][0])
+                self.assembly_solver_time[dataset][-1][0] = (self.assembly_solver_time[dataset][-1][0] +
+                                                    float(record.assembly_time)) / (self.iterations_per_ts[dataset][-1])
             elif isinstance(record, ogs6py.ogs_regexes.ogs_regexes.LinearSolverTime):
-                self.assembly_solver_time[-1][1] = (self.assembly_solver_time[-1][1] +
-                                                   record.linear_solver_time) / (1. + self.iterations_per_ts[-1])
-                print(self.assembly_solver_time[-1][1])
+                self.assembly_solver_time[dataset][-1][1] = (self.assembly_solver_time[dataset][-1][1] +
+                                                   float(record.linear_solver_time)) / (self.iterations_per_ts[dataset][-1])
             elif isinstance(record, ogs6py.ogs_regexes.ogs_regexes.ComponentConvergenceCriterion):
                 if record.component == int(self.crit_comp):
-                    self.criterion.append((self.timesteps[-1], self.iterations_per_ts, getattr(record, self.crit)))
-        self.last_record = len_records
+                    if int(self.iterations_per_ts[dataset][-1]) == 1:
+                        self.criterion_x[dataset].append(len(self.criterion_x[dataset])+1)
+                        self.criterion_label[dataset].append(f"{self.iterations_per_ts[dataset][-1]} \n{self.timesteps[dataset][-1]}")
+                        self.criterion_y[dataset].append(float(getattr(record, self.crit)))
+                        self.crit_timesteppointer[dataset].append(len(self.criterion_x[dataset])-1)
+                    else:
+                        self.criterion_x[dataset].append(len(self.criterion_x[dataset])+1)
+                        self.criterion_label[dataset].append(f"{self.iterations_per_ts[dataset][-1]} \n")
+                        self.criterion_y[dataset].append(float(getattr(record, self.crit)))
+                    #self.criterion[dataset].append((self.timesteps[dataset][-1], self.iterations_per_ts[dataset], getattr(record, self.crit)))
+        #self.last_record[dataset] = len_records
 
 
     def plot_init(self):
@@ -132,7 +145,11 @@ class OGSMONITOR(object):
             print(m)
             logfile_df = {}
             start = time.time()
-            records, self.last_line = parser.parse_file(self.log_file, maximum_lines=self.maximum_lines, force_parallel=False, start_line=self.last_line)
+            records, self.last_line["actual"] = parser.parse_file(
+                    self.log_file,
+                    maximum_lines=self.maximum_lines,
+                    force_parallel=False,
+                    start_line=self.last_line["actual"])
             stop = time.time()
             print(f"parse {stop-start} s")
             start = time.time()
@@ -169,6 +186,11 @@ class OGSMONITOR(object):
             stop = time.time()
             print(f"transform/filter df {stop-start} s")
             start = time.time()
+            window_length = self.window_length
+            if len(self.timesteps["actual"]) < self.window_length:
+                window_length = len(self.timesteps["actual"])
+            ts0 = int(np.array(self.timesteps["actual"])[-window_length])-1
+            ts1 = int(np.array(self.timesteps["actual"])[-1])
             if ((self.show_quadrant == -1) or (self.show_quadrant == 1)):
                 if self.show_quadrant == -1:
                     ax = self.axs[0,0]
@@ -180,14 +202,15 @@ class OGSMONITOR(object):
                 """
                 ax.plot(logfile_df[0]["time_step"], logfile_df[0]["iteration_number"], 'b', label="actual log")
                 """
-                ax.plot(np.array(self.timesteps), np.array(self.iterations_per_ts), 'b', label="actual log")
+                ax.plot(np.array(self.timesteps["actual"])[-window_length:], np.array(self.iterations_per_ts["actual"])[-window_length:], 'b', label="actual log")
                 if self.q1_yb is None:
                     self.q1_yb = ax.twiny()
                     self.q1_yb.get_legend_handles_labels()
                     #self.q1_yb.get_yaxis().set_visible(False)
-                if not self.logfile_ref_df[0] is None:
+                if not self.ref_file is None:
                     #self.q1_yb.get_yaxis().set_visible(False)
-                    self.q1_yb.plot(self.logfile_ref_df[0]["time_step"], self.logfile_ref_df[0]["iteration_number"], 'k', label="reference data")
+                    self.q1_yb.clear()
+                    self.q1_yb.plot(np.array(self.timesteps["ref"])[ts0:ts1], np.array(self.iterations_per_ts["ref"])[ts0:ts1], 'k', label="reference data")
                 ax.set(xlabel="time step", ylabel="iterations")
                 lines, labels = self.q1_yb.get_legend_handles_labels()
                 lines2, labels2 = ax.get_legend_handles_labels()
@@ -205,19 +228,20 @@ class OGSMONITOR(object):
                 """
                 ax.plot(logfile_df[1]["time_step"], logfile_df[1]["step_size"], 'b-', label="time step (actual)")
                 """
-                ax.plot(np.array(self.timesteps), np.array(self.ts_simtime)[:,1], 'b-', label="time step (actual)")
+                ax.plot(np.array(self.timesteps["actual"])[-window_length:], np.array(self.ts_simtime["actual"])[:,1][-window_length:], 'b-', label="time step (actual)")
                 if self.q0_yb is None:
                     self.q0_yb = ax.twiny()
                     #self.q0_yb.get_yaxis().set_visible(False)
-                if not self.logfile_ref_df[1] is None:
+                if not self.ref_file is None:
                     #self.q0_yb.get_yaxis().set_visible(True)
-                    self.q0_yb.plot(self.logfile_ref_df[1]["time_step"], self.logfile_ref_df[1]["step_size"], 'k-', label="time step (reference)")
+                    self.q0_yb.clear()
+                    self.q0_yb.plot(np.array(self.timesteps["ref"])[ts0:ts1], np.array(self.ts_simtime["ref"])[:,1][ts0:ts1], 'k-', label="time step (reference)")
                 if self.q0_xb is None:
                     self.q0_xb = ax.twinx()
                 """
                 self.q0_xb.plot(logfile_df[1]["time_step"], logfile_df[1]["step_start_time"], 'r-', label="start time (actual)")
                 """
-                self.q0_xb.plot(np.array(self.timesteps), np.array(self.ts_simtime)[:,0], 'r-', label="start time (actual)")
+                self.q0_xb.plot(np.array(self.timesteps["actual"])[-window_length:], np.array(self.ts_simtime["actual"])[:,0][-window_length:], 'r-', label="start time (actual)")
                 self.q0_xb.set_ylabel("time / s", color="r")
                 self.q0_xb.yaxis.set_label_position("right")
                 lines, labels = self.q0_yb.get_legend_handles_labels()
@@ -238,15 +262,16 @@ class OGSMONITOR(object):
                 ax.plot(logfile_df[1]["time_step"],logfile_df[1]["assembly_time"],"b-", label="assembly time (actual)")
                 ax.plot(logfile_df[1]["time_step"],logfile_df[1]["linear_solver_time"], "g-", label="linear solver time (actual)")
                 """
-                ax.plot(np.array(self.timesteps), np.array(self.assembly_solver_time)[:,0],"b-", label="assembly time (actual)")
-                ax.plot(np.array(self.timesteps), np.array(self.assembly_solver_time)[:,1], "g-", label="linear solver time (actual)")
+                ax.plot(np.array(self.timesteps["actual"])[-window_length:], np.array(self.assembly_solver_time["actual"])[:,0][-window_length:],"b-", label="assembly time (actual)")
+                ax.plot(np.array(self.timesteps["actual"])[-window_length:], np.array(self.assembly_solver_time["actual"])[:,1][-window_length:], "g-", label="linear solver time (actual)")
                 if self.q2_yb is None:
                     self.q2_yb = ax.twiny()
                     #self.q2_yb.get_yaxis().set_visible(False)
-                if not self.logfile_ref_df[1] is None:
+                if not self.ref_file is None:
                     #self.q2_yb.get_yaxis().set_visible(True)
-                    self.q2_yb.plot(self.logfile_ref_df[1]["time_step"], self.logfile_ref_df[1]["assembly_time"], "b--", label="assembly time (ref)")
-                    self.q2_yb.plot(self.logfile_ref_df[1]["time_step"], self.logfile_ref_df[1]["linear_solver_time"], "g--", label="linear solver time (ref)")
+                    self.q2_yb.clear()
+                    self.q2_yb.plot(self.timesteps["ref"][ts0:ts1], np.array(self.assembly_solver_time["ref"])[:,0][ts0:ts1], "b--", label="assembly time (ref)")
+                    self.q2_yb.plot(self.timesteps["ref"][ts0:ts1], np.array(self.assembly_solver_time["ref"])[:,1][ts0:ts1], "g--", label="linear solver time (ref)")
                 lines, labels = self.q2_yb.get_legend_handles_labels()
                 lines2, labels2 = ax.get_legend_handles_labels()
                 ax.legend(lines + lines2, labels + labels2, loc=0)
@@ -254,6 +279,7 @@ class OGSMONITOR(object):
                 ax.set_title("time step sizes")
 
             if ((self.show_quadrant == -1) or (self.show_quadrant == 3)):
+                """
                 start2 = time.time()
                 newindex = []
                 for i in logfile_df[2][logfile_df[2]["component"]==self.crit_comp].index:
@@ -298,32 +324,58 @@ class OGSMONITOR(object):
                         tmp = i
                 stop2 = time.time()
                 print(f"index trafo {stop2-start2} s")
+                """
+                line_begin = self.crit_timesteppointer['actual'][ts0]
+                line_begin_ref = self.crit_timesteppointer['ref'][ts0]
+                line_stop_ref = line_begin_ref
+                if len(self.crit_timesteppointer['ref']) <= ts1:
+                    ts1 = len(self.crit_timesteppointer['ref']) - 1
+                    line_stop_ref = len(self.criterion_x['ref'])
+                else:
+                    line_stop_ref = self.crit_timesteppointer['ref'][ts1-1]
+                def update_ticks(x, pos):
+                    if pos is None:
+                        return ""
+                    else:
+                        it_num = self.criterion_label["actual"][line_begin:][pos]
+                        return f"{it_num}"
+                def update_ticks_ref(x, pos):
+                    if pos is None:
+                        return ""
+                    else:
+                        it_num = self.criterion_label["ref"][line_begin_ref:line_stop_ref][pos]
+                        return f"{it_num}"
                 if self.show_quadrant == -1:
                     ax = self.axs[1,1]
                 else:
                     ax = self.axs
                 ax.clear()
-                ax.plot(newindex, logfile_df[2][logfile_df[2]["component"]==self.crit_comp][self.crit], 'b', label="actual")
+                #ax.plot(newindex, logfile_df[2][logfile_df[2]["component"]==self.crit_comp][self.crit], 'b', label="actual")
+                ax.plot(np.array(self.criterion_x["actual"])[line_begin:], np.array(self.criterion_y["actual"])[line_begin:],"b-", label="criterion (actual)")
                 if self.q3_yb is None:
                     self.q3_yb = ax.twiny()
                     #self.q3_yb.get_yaxis().set_visible(False)
-                if not self.logfile_ref_df[2] is None:
+                if not self.ref_file is None:
+                    self.q3_yb.clear()
                     #self.q3_yb.get_yaxis().set_visible(True)
-                    self.q3_yb.plot(newindex_ref, self.logfile_ref_df[2][self.logfile_ref_df[2]["component"]==self.crit_comp][self.crit], 'k', label="reference")
+                    self.q3_yb.plot(np.array(self.criterion_x["ref"])[line_begin_ref:line_stop_ref], np.array(self.criterion_y["ref"])[line_begin_ref:line_stop_ref], 'k', label="reference")
+                    self.q3_yb.set_xticks(np.array(self.criterion_x["actual"][line_begin_ref:line_stop_ref]))
                     self.q3_yb.xaxis.set_major_formatter(mticker.FuncFormatter(update_ticks_ref))
-                    sec_ref = self.q3_yb.secondary_xaxis(location=1)
-                    sec_ref.set_xticks(newindex2_ref, labels=time_data_ref)
-                    sec_ref.tick_params('x', length=0)
+                    #sec_ref = self.q3_yb.secondary_xaxis(location=1)
+                    #sec_ref.set_xticks(newindex2_ref, labels=time_data_ref)
+                    #sec_ref.tick_params('x', length=0)
                 lines, labels = self.q3_yb.get_legend_handles_labels()
                 lines2, labels2 = ax.get_legend_handles_labels()
                 ax.legend(lines + lines2, labels + labels2, loc=0)
-                ax.set(xlabel="\n\ntime step", ylabel=self.crit)
+                ax.set(xlabel="\niterations, time step", ylabel=self.crit)
                 ax.set_title("criterion")
+                ax.tick_params('x', which='both', length=0)
+                ax.set_xticks(np.array(self.criterion_x["actual"][line_begin:]))
                 ax.xaxis.set_major_formatter(mticker.FuncFormatter(update_ticks))
                 ax.set_yscale('log')
-                sec = ax.secondary_xaxis(location=0)
-                sec.set_xticks(newindex2, labels=time_data)
-                sec.tick_params('x', length=0)
+                #sec = ax.secondary_xaxis(location=0)
+                #sec.set_xticks(newindex2, labels=time_data)
+                #sec.tick_params('x', length=0)
             if self.running is True:
                 try:
                     self.fig.suptitle(f"OGS running\n({logfile_df[1]['time_step_solution_time'].sum()} s)")
